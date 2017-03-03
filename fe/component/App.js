3d8686555dd25ee4dc96c0f06d620bd6876723ff
serm-tape <serm.thm@gmail.com>
2017-03-03 18:08:19 +0700

    add fblogin wrapper

--- fe/component/App.js
+++ fe/component/App.js
@@ -1,11 +1,62 @@
 import React, {Component} from 'react'
 
 class App extends Component{
+
+    constructor(props){
+        super(props)
+        state = {
+            fbSdkLoaded: false,
+            fbId: null,
+            fbToken: null,
+        }
+    }
+
+    //define FB.asyncInit()
+    //check if loggedin=>setState(id,token)
+    //load fbsdk
+    componentDidMount(){
+        window.fbAsyncInit = () => {
+            FB.init({
+                appId: '',
+                xfbml: true,
+                version: 'v2.8',
+            })
+            FB.AppEvents.logPageView();
+            this.setState({fbSdkLoaded:true})
+
+            //if not login do login, then get id wheter login or not
+            FB.getLoginStatus( resp => {
+                Fb.login( loginResp => {
+                    this.setState({fbId:loginResp.auth_response.userID, fbToken:loginResp.auth_response.token})
+                })
+            })
+        }
+
+        const loadAsync = (d, id) => {
+            let js, fjs = d.getElmentsByTagName('script')[0]
+            if (d.getElementById(id)){return;}
+            js = d.CreateElement(s)
+            js.id = id
+            js.src = '//connect.facebook.net/en_US/sdk.js'
+            fjs.parentNode.insertBefore(js, fjs)
+        }
+        loadAsync(document, 'facebook-jssdk')
+    }
+
     render(){
+        let content = null
+        if (!fbSdkLoaded){
+            //content = (<p>Initializing</p>)
+        }else if(!fbId){
+            //content = (<FBLoginButton />)
+        }else{
+            //content = this.props.children
+        }
         return (
             <div>
-                <h1> This is app. </h1>
-                {this.props.children}
+                <Header />
+                {content}
+                <Footer />
             </div>
         )
     }
